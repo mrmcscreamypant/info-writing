e62a08a539f5d4c622e226aeffbacd1cfeb991b7
jrs <nil>
2025-12-09 18:34:42 -0500

    Better PWA

--- src/pages/Narrative.tsx
+++ src/pages/Narrative.tsx
@@ -6,11 +6,13 @@ import YAMLParser from './YAMLParser';
 import CodeSample, { CodeLanguage } from '../widgets/CodeSample';
 
 import cursedCodeSample from '../widgets/Paragraph?raw';
+import inceptionCodeSample from '../widgets/CodeSample?raw';
 
 export default function Narrative(): React.JSX.Element {
     return <Page>
         <YAMLParser markup={document} figs={{
-            cursed: <CodeSample content={cursedCodeSample} language={CodeLanguage.TS} file='Paragraph.tsx' />
+            cursed: <CodeSample content={cursedCodeSample} language={CodeLanguage.TS} file='Paragraph.tsx' />,
+            inception: <CodeSample content={inceptionCodeSample} language={CodeLanguage.TS} file='CodeSample.tsx' />
         }} />
     </Page>;
 }
