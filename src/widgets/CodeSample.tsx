7fbc36c9a48dc523a2f0e456e3ec9d2566aaea65
jrs <nil>
2025-12-09 17:58:36 -0500

    NOW the crash is fixed

--- src/widgets/CodeSample.tsx
+++ src/widgets/CodeSample.tsx
@@ -2,7 +2,16 @@ import React, { useEffect } from 'react';
 import * as monaco from 'monaco-editor';
 import { v4 } from 'uuid';
 import './code-sample.css';
-import { AspectRatio, Card } from '@radix-ui/themes';
+import { AspectRatio, Box, Card } from '@radix-ui/themes';
+
+monaco.typescript.typescriptDefaults.setCompilerOptions({
+    jsx: monaco.typescript.JsxEmit.ReactJSX
+});
+
+monaco.typescript.typescriptDefaults.setDiagnosticsOptions({
+    noSemanticValidation: false,
+    noSyntaxValidation: false
+});
 
 export const enum CodeLanguage {
     JS = 'javascript',
@@ -10,16 +19,22 @@ export const enum CodeLanguage {
     PY = 'python'
 }
 
-export default function CodeSample({ content, language = CodeLanguage.PY, file = "_.py" }: { content: string, language?: CodeLanguage, file?: string }): React.JSX.Element {
+export default function CodeSample({ content, language = CodeLanguage.PY, file }: { content: string, language?: CodeLanguage, file?: string }): React.JSX.Element {
     const uuid = v4();
 
     useEffect(() => {
         const elem = document.getElementById(uuid);
+        const uri = file ? monaco.Uri.file(file) : null;
+        let model;
+        try {
+            model = monaco.editor.createModel(content, language, uri);
+        } catch (e) {
+            model = monaco.editor.getModel(uri);
+        }
 
         monaco.editor.create(elem, {
             readOnly: true,
-            language: language,
-            value: content,
+            model: model,
             roundedSelection: true,
             scrollbar: {
                 vertical: "hidden",
