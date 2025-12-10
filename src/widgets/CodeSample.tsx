54b35d1aa0f6c4a9657b48baf4210d5d4e9e7bff
jrs <nil>
2025-12-09 19:05:45 -0500

    Now the workers should be fixed

--- src/widgets/CodeSample.tsx
+++ src/widgets/CodeSample.tsx
@@ -2,16 +2,31 @@ import React, { useEffect } from 'react';
 import * as monaco from 'monaco-editor';
 import { v4 } from 'uuid';
 import './code-sample.css';
-import { AspectRatio, Box, Card } from '@radix-ui/themes';
+import { AspectRatio, Card } from '@radix-ui/themes';
 
-monaco.typescript.typescriptDefaults.setCompilerOptions({
-    jsx: monaco.typescript.JsxEmit.ReactJSX
-});
+import editorWorker from 'monaco-editor/esm/vs/editor/editor.worker?worker';
+import jsonWorker from 'monaco-editor/esm/vs/language/json/json.worker?worker';
+import cssWorker from 'monaco-editor/esm/vs/language/css/css.worker?worker';
+import htmlWorker from 'monaco-editor/esm/vs/language/html/html.worker?worker';
+import tsWorker from 'monaco-editor/esm/vs/language/typescript/ts.worker?worker';
 
-monaco.typescript.typescriptDefaults.setDiagnosticsOptions({
-    noSemanticValidation: false,
-    noSyntaxValidation: false
-});
+self.MonacoEnvironment = {
+    getWorker(_, label): Worker {
+        if (label === 'json') {
+            return new jsonWorker();
+        }
+        if (label === 'css' || label === 'scss' || label === 'less') {
+            return new cssWorker();
+        }
+        if (label === 'html' || label === 'handlebars' || label === 'razor') {
+            return new htmlWorker();
+        }
+        if (label === 'typescript' || label === 'javascript') {
+            return new tsWorker();
+        }
+        return new editorWorker();
+    }
+};
 
 export const enum CodeLanguage {
     JS = 'javascript',
--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,5 @@
 import { defineConfig } from 'vite';
-import monacoEditorPlugin from 'vite-plugin-monaco-editor';
 
 export default defineConfig({
-    base: "https://mrmcscreamypant.github.io/info-writing/",
-    plugins: [monacoEditorPlugin({ publicPath: "/info-writing" })]
+    base: "https://mrmcscreamypant.github.io/info-writing/"
 });
