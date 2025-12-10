43fa88e8c78981396603851eee0e8612b0c84d79
jrs <nil>
2025-12-09 20:37:21 -0500

    don't need all of that

--- src/index.tsx
+++ src/index.tsx
@@ -1,16 +1,6 @@
 import { createRoot } from 'react-dom/client';
 import { App } from './core/App';
 
-type BeforeInstallPromptEvent = { prompt: () => void } & Event;
-let deferredPrompt: BeforeInstallPromptEvent;
-window.addEventListener('beforeinstallprompt', (e) => {
-    e.preventDefault();
-    deferredPrompt = e as BeforeInstallPromptEvent;
-    console.log(e);
-});
-
-window.addEventListener("click", () => { deferredPrompt.prompt(); });
-
 const elem = document.getElementById("root");
 if (!elem) { throw new Error("Could not find root element!"); }
 const root = createRoot(elem);
