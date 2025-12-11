5d812c020db607a094d0aedfc38bffade7a7f9b0
jrs <nil>
2025-12-11 10:16:47 -0500

    Sample push as a demo

--- src/pages/Narrative.tsx
+++ src/pages/Narrative.tsx
@@ -5,14 +5,12 @@ import document from './narrative.yaml?raw';
 import YAMLParser from './YAMLParser';
 import CodeSample, { CodeLanguage } from '../widgets/CodeSample';
 
-import cursedCodeSample from '../widgets/Paragraph?raw';
-import inceptionCodeSample from '../widgets/CodeSample?raw';
+import spaghettiCodeSample from '../background/Engine?raw';
 
 export default function Narrative(): React.JSX.Element {
     return <Page>
         <YAMLParser markup={document} figs={{
-            cursed: <CodeSample content={cursedCodeSample} language={CodeLanguage.TS} file='Paragraph.tsx' />,
-            inception: <CodeSample content={inceptionCodeSample} language={CodeLanguage.TS} file='CodeSample.tsx' />
+            spaghetti: <CodeSample content={spaghettiCodeSample} language={CodeLanguage.TS} file='Engine.ts' />,
         }} />
     </Page>;
 }
