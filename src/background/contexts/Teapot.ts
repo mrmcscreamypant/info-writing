832381d741de3716060181209088efd1062affbf
jrs <nil>
2025-12-12 14:12:45 -0500

    How many teapots are too many? (A: never)

--- src/background/contexts/Teapot.ts
+++ src/background/contexts/Teapot.ts
@@ -7,12 +7,27 @@ import { TeapotGeometry } from 'three/addons/geometries/TeapotGeometry.js';
 import LightArray from '../LightArray';
 
 class TeapotEntity extends Entity {
+    private readonly hundreds: THREE.Group;
+    private readonly INSTANCE_COUNT: number = 800;
+
     public constructor(engine: Engine) {
         super(engine);
 
-        this.add(new THREE.Mesh(new TeapotGeometry(0.5), new THREE.MeshPhongMaterial({
-            side: THREE.DoubleSide
-        })));
+        const mesh = new THREE.Mesh(
+            new TeapotGeometry(0.5),
+            new THREE.MeshPhongMaterial({
+                side: THREE.DoubleSide,
+            })
+        );
+        this.add(mesh);
+
+        this.hundreds = new THREE.Group();
+        for (let i = 0; i < this.INSTANCE_COUNT; i++) {
+            const hundred = mesh.clone();
+            hundred.scale.setScalar(0.1);
+            hundred.position.random().subScalar(0.5).multiplyScalar(10);
+            this.hundreds.add(hundred);
+        }
     }
 
     public tick(delta: number): void {
@@ -21,6 +36,14 @@ class TeapotEntity extends Entity {
         this.rotation.x += gValue * 1.5;
         this.rotation.y += gValue;
         this.rotation.z += gValue * 0.5;
+
+        if (this.engine.hooks.scrollProgress.get() > 0.94) {
+            if (!this.children.includes(this.hundreds)) {
+                this.add(this.hundreds);
+            }
+        } else if (this.children.includes(this.hundreds)) {
+            this.remove(this.hundreds);
+        }
     }
 }
 
