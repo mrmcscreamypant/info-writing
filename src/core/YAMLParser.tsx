94b267b35425c88464baf2752a055592e1be0835
mrmcscreamypant <161328176+mrmcscreamypant@users.noreply.github.com>
2025-12-18 16:29:36 +0000

    abstract that into it's own element, you dunce!

--- src/core/YAMLParser.tsx
+++ src/core/YAMLParser.tsx
@@ -8,7 +8,7 @@ import { AnimatePresence, motion } from 'motion/react';
 import * as Icons from '@radix-ui/react-icons';
 
 type Def = { term: string, meaning: string };
-type YAMLParagraph = { title?: string, content: string, fig?: null, def?: Def[] };
+type YAMLParagraph = { title?: string, content: string, fig?: null, defs?: Def[] };
 type YAMLPage = YAMLParagraph[];
 
 function Definition({ term, meaning }: Def): React.JSX.Element {
@@ -18,42 +18,47 @@ function Definition({ term, meaning }: Def): React.JSX.Element {
     </>;
 }
 
-export default function YAMLParser({ markup, figs }: { markup: string, figs?: { [key: string]: React.JSX.Element } }): React.JSX.Element[] {
+function DefinitionList({ defs }: { defs: Def[] }): React.JSX.Element {
     const [defHovered, setDefHovered] = React.useState(false);
+
+    return <motion.div
+        onHoverStart={() => setDefHovered(true)}
+        onHoverEnd={() => setDefHovered(false)}
+        style={{
+            minWidth: "20%",
+            maxHeight: "100%",
+            minHeight: "100%",
+            alignContent: "center",
+            textAlign: "center"
+        }}
+    >
+        {defHovered ? <motion.div
+            initial={{ opacity: 0 }}
+            animate={{ opacity: 1 }}
+        >
+            <Card>
+                {defs.flatMap(
+                    (def, j: number) => <Definition
+                        key={j}
+                        term={def.term}
+                        meaning={def.meaning}
+                    />
+                )}
+            </Card>
+        </motion.div> : <IconButton radius="full">
+            <Icons.AccessibilityIcon width="18" height="18" />
+        </IconButton>}
+    </motion.div>;
+}
+
+export default function YAMLParser({ markup, figs }: { markup: string, figs?: { [key: string]: React.JSX.Element } }): React.JSX.Element[] {
     const content = load(markup) as YAMLPage;
 
     return content.flatMap(
         (paragraph, i: number) => <Paragraph key={i} title={paragraph.title}>
             <Flex>
                 {paragraph.fig ? figs[paragraph.fig] : <Text>{paragraph.content}</Text>}
-                {paragraph.def && <motion.div
-                    onHoverStart={() => setDefHovered(true)}
-                    onHoverEnd={() => setDefHovered(false)}
-                    style={{
-                        minWidth: "20%",
-                        maxHeight: "100%",
-                        minHeight: "100%",
-                        alignContent: "center",
-                        textAlign: "center"
-                    }}
-                >
-                    {defHovered ? <motion.div
-                        initial={{ opacity: 0 }}
-                        animate={{ opacity: 1 }}
-                    >
-                        <Card>
-                            {paragraph.def.flatMap(
-                                (def, j: number) => <Definition
-                                    key={j}
-                                    term={def.term}
-                                    meaning={def.meaning}
-                                />
-                            )}
-                        </Card>
-                    </motion.div> : <IconButton radius="full">
-                        <Icons.AccessibilityIcon width="18" height="18"/>
-                    </IconButton>}
-                </motion.div>}
+                {paragraph.defs && <DefinitionList defs={paragraph.defs} />}
             </Flex>
         </Paragraph >
     );
