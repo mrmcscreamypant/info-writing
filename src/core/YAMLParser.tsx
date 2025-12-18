515b0be13ddcb4a37d5d82829b280fb221a276d1
mrmcscreamypant <161328176+mrmcscreamypant@users.noreply.github.com>
2025-12-18 16:05:05 +0000

    more stuff

--- src/core/YAMLParser.tsx
+++ src/core/YAMLParser.tsx
@@ -3,17 +3,17 @@ import { load } from 'js-yaml';
 import Paragraph from '../widgets/Paragraph';
 import { Badge, Card, Flex, Text } from '@radix-ui/themes';
 
-import motion
+import { motion } from 'motion/react';
 
 type Def = { term: string, meaning: string };
 type YAMLParagraph = { title?: string, content: string, fig?: null, def?: Def[] };
 type YAMLPage = YAMLParagraph[];
 
 function Definition({ term, meaning }: Def): React.JSX.Element {
-    return <Card size="2">
+    return <>
         <Badge>{term}</Badge><br />
         <Text>{meaning}</Text>
-    </Card>;
+    </>;
 }
 
 export default function YAMLParser({ markup, figs }: { markup: string, figs?: { [key: string]: React.JSX.Element } }): React.JSX.Element[] {
@@ -23,15 +23,18 @@ export default function YAMLParser({ markup, figs }: { markup: string, figs?: { 
         (paragraph, i: number) => <Paragraph key={i} title={paragraph.title}>
             <Flex>
                 {paragraph.fig ? figs[paragraph.fig] : <Text>{paragraph.content}</Text>}
-                <motion.div>
-                {paragraph.def && paragraph.def.flatMap(
-                    (def, j: number) => <Definition
-                        key={j}
-                        term={def.term}
-                        meaning={def.meaning}
-                    />
-                )}
-                </motion.div>
+                {paragraph.def && <motion.div>
+                    <Card>
+                        {
+                            paragraph.def.flatMap(
+                                (def, j: number) => <Definition
+                                    key={j}
+                                    term={def.term}
+                                    meaning={def.meaning}
+                                />)
+                        }
+                    </Card>
+                </motion.div>}
             </Flex>
         </Paragraph>
     );
