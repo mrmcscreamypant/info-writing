ab346270f43c951137a904493cd1fd8a78607e4d
mrmcscreamypant <161328176+mrmcscreamypant@users.noreply.github.com>
2025-12-18 16:01:38 +0000

    semi-functional definitions

--- src/core/YAMLParser.tsx
+++ src/core/YAMLParser.tsx
@@ -1,18 +1,38 @@
 import React from 'react';
 import { load } from 'js-yaml';
 import Paragraph from '../widgets/Paragraph';
-import { Text } from '@radix-ui/themes';
+import { Badge, Card, Flex, Text } from '@radix-ui/themes';
 
+import motion
 
-type YAMLParagraph = { title?: string, content: string, fig?: null };
-type YAMLPage = (YAMLParagraph)[];
+type Def = { term: string, meaning: string };
+type YAMLParagraph = { title?: string, content: string, fig?: null, def?: Def[] };
+type YAMLPage = YAMLParagraph[];
+
+function Definition({ term, meaning }: Def): React.JSX.Element {
+    return <Card size="2">
+        <Badge>{term}</Badge><br />
+        <Text>{meaning}</Text>
+    </Card>;
+}
 
 export default function YAMLParser({ markup, figs }: { markup: string, figs?: { [key: string]: React.JSX.Element } }): React.JSX.Element[] {
     const content = load(markup) as YAMLPage;
 
     return content.flatMap(
         (paragraph, i: number) => <Paragraph key={i} title={paragraph.title}>
-            {paragraph.fig ? figs[paragraph.fig] : <Text>{paragraph.content}</Text>}
+            <Flex>
+                {paragraph.fig ? figs[paragraph.fig] : <Text>{paragraph.content}</Text>}
+                <motion.div>
+                {paragraph.def && paragraph.def.flatMap(
+                    (def, j: number) => <Definition
+                        key={j}
+                        term={def.term}
+                        meaning={def.meaning}
+                    />
+                )}
+                </motion.div>
+            </Flex>
         </Paragraph>
     );
 }
