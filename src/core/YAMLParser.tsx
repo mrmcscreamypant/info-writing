eb900b2f52b26839da4dda936556b615bb191006
mrmcscreamypant <161328176+mrmcscreamypant@users.noreply.github.com>
2025-12-18 16:20:30 +0000

    accessability

--- src/core/YAMLParser.tsx
+++ src/core/YAMLParser.tsx
@@ -1,9 +1,11 @@
 import React from 'react';
 import { load } from 'js-yaml';
 import Paragraph from '../widgets/Paragraph';
-import { Badge, Card, Flex, Text } from '@radix-ui/themes';
+import { Badge, Card, Flex, IconButton, Text } from '@radix-ui/themes';
 
-import { motion } from 'motion/react';
+import { AnimatePresence, motion } from 'motion/react';
+
+import * as Icons from '@radix-ui/react-icons';
 
 type Def = { term: string, meaning: string };
 type YAMLParagraph = { title?: string, content: string, fig?: null, def?: Def[] };
@@ -17,25 +19,42 @@ function Definition({ term, meaning }: Def): React.JSX.Element {
 }
 
 export default function YAMLParser({ markup, figs }: { markup: string, figs?: { [key: string]: React.JSX.Element } }): React.JSX.Element[] {
+    const [defHovered, setDefHovered] = React.useState(false);
     const content = load(markup) as YAMLPage;
 
     return content.flatMap(
         (paragraph, i: number) => <Paragraph key={i} title={paragraph.title}>
             <Flex>
                 {paragraph.fig ? figs[paragraph.fig] : <Text>{paragraph.content}</Text>}
-                {paragraph.def && <motion.div>
-                    <Card>
-                        {
-                            paragraph.def.flatMap(
+                {paragraph.def && <motion.div
+                    onHoverStart={() => setDefHovered(true)}
+                    onHoverEnd={() => setDefHovered(false)}
+                    style={{
+                        minWidth: "20%",
+                        maxHeight: "100%",
+                        minHeight: "100%",
+                        alignContent: "center",
+                        textAlign: "center"
+                    }}
+                >
+                    {defHovered ? <motion.div
+                        initial={{ opacity: 0 }}
+                        animate={{ opacity: 1 }}
+                    >
+                        <Card>
+                            {paragraph.def.flatMap(
                                 (def, j: number) => <Definition
                                     key={j}
                                     term={def.term}
                                     meaning={def.meaning}
-                                />)
-                        }
-                    </Card>
+                                />
+                            )}
+                        </Card>
+                    </motion.div> : <IconButton radius="full">
+                        <Icons.AccessibilityIcon width="18" height="18"/>
+                    </IconButton>}
                 </motion.div>}
             </Flex>
-        </Paragraph>
+        </Paragraph >
     );
 }
