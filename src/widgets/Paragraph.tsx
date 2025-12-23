e5199cc4ae4f59dab2ec1cf83481b55ef7ff916e
jrs <nil>
2025-12-23 09:45:29 -0500

    I hope this actually is a perfomance improvement

--- src/widgets/Paragraph.tsx
+++ src/widgets/Paragraph.tsx
@@ -2,7 +2,7 @@ import { Box, Heading, Section, Separator } from '@radix-ui/themes';
 import React from 'react';
 import './paragraph.css';
 
-import { motion, transformValue, useScroll } from 'motion/react';
+import { motion, transformValue, useScroll, useMotionValueEvent } from 'motion/react';
 
 export default function Paragraph({ title, children }: { title?: string } & React.PropsWithChildren): React.JSX.Element {
     const paragraphRef = React.useRef(null) as React.RefObject<HTMLDivElement>;
@@ -15,14 +15,15 @@ export default function Paragraph({ title, children }: { title?: string } & Reac
 
     return <motion.div ref={paragraphRef} style={{
         transform: transformValue(() => `scale(1,${anim.get()})`),
-        opacity: anim
+        opacity: anim,
+        visibility: transformValue(() => scrollYProgress.get() > 0 ? 'visible' : 'hidden')
     }}>
         <Section size={"1"}>
             {
-                title ? <>
+                title && <>
                     <Heading color="teal">{title}</Heading>
                     <Separator my="3" size="4" />
-                </> : null
+                </>
             }
             <Box className="paragraph">
                 {children}
