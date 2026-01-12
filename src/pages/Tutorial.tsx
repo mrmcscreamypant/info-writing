ce0af3bf64d41ad827d000ca782a757cbe056cf9
mrmcscreamypant <161328176+mrmcscreamypant@users.noreply.github.com>
2026-01-12 18:48:18 +0000

    partial tutorial

--- src/pages/Tutorial.tsx
+++ src/pages/Tutorial.tsx
@@ -3,9 +3,22 @@ import Page from '../core/Page';
 import YAMLParser from '../core/YAMLParser';
 
 import markup from './tutorial.yaml?raw';
+import CodeSample, { CodeLanguage } from '../widgets/CodeSample';
+
+import echo from './codeSamples/echo.py?raw';
+import cipher from './codeSamples/cipher.py?raw';
+
+function cipherLines(end: number): string {
+    return cipher.split("\n").slice(0, end).join("\n");
+}
 
 export default function Tutorial(): React.JSX.Element {
     return <Page>
-        <YAMLParser markup={markup} />
+        <YAMLParser markup={markup} figs={{
+            helloworld: <CodeSample content='print("Hello, world!")' language={CodeLanguage.PY} file='helloWorld.py' />,
+            echo: <CodeSample content={echo} language={CodeLanguage.PY} file='echo.py' />,
+            variable: <CodeSample content={cipherLines(1)} language={CodeLanguage.PY} file='variable.py' />,
+            loop: <CodeSample content={cipherLines(5)} language={CodeLanguage.PY} file='loop.py' />,
+        }} />
     </Page>;
 }
---
+++ src/pages/codeSamples/cipher.py
@@ -0,0 +1,7 @@
+plaintext = input("> ")
+
+output = ""
+for character in plaintext:
+    output = character + plaintext
+
+print(output)
