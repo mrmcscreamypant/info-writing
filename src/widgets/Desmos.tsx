709415f4df16f7bb737ed25684c35b8e04f9947c
jrs <nil>
2026-02-02 09:52:48 -0500

    oops

--- src/widgets/Desmos.tsx
+++ src/widgets/Desmos.tsx
@@ -1,11 +1,14 @@
 import { AspectRatio } from '@radix-ui/themes';
 import React from 'react';
+import Desmos
 
 export const enum DesmosType {
     CALCULATOR = "calculator",
     GEOMETRY = "geometry",
     THREED = "3d",
 }
+
+const API_KEY = "8b3dceaf9fc446a38bf40c6e805cafd9";
 
 export default function Desmos({ slug, type, embed }: { slug: string, type: DesmosType, embed?: boolean }): React.JSX.Element {
     return <AspectRatio ratio={1.5}>
